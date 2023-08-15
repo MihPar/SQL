0183968fdc6da8714e494b12b319179a0b4265df
MihPar <9496260@gmail.com>
2023-08-15 16:24:01 +0300

    c_w08

---
+++ code_warsSQL/c_w08.sql
@@ -0,0 +1,5 @@
+INSERT INTO participants(name, age, attending)
+VALUES('Mikhael', 41, true);
+
+SELECT * FROM participants;
+
--- table01.sql
+++ table01.sql
@@ -40,6 +40,52 @@ WHERE id = 1
 UPDATE superheroes
 SET gender = "Man"
 WHERE gender = "Male Characters"
+---------------------------------------
+1. Создание таблицы:
+
+```sql
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  age INT
+);
+```
+
+2. Вставка данных в таблицу:
+
+```sql
+INSERT INTO users (id, name, age)
+VALUES (1, 'John', 25), (2, 'Jane', 30), (3, 'Bob', 35);
+```
+
+3. Выбор всех записей из таблицы:
+
+```sql
+SELECT * FROM users;
+```
+
+4. Выбор определенных полей и условий:
+
+```sql
+SELECT name, age FROM users 
+WHERE age > 25;
+```
+
+5. Обновление данных в таблице:
+
+```sql
+UPDATE users 
+SET age = 40 
+WHERE id = 1;
+```
+
+6. Удаление данных из таблицы:
+
+```sql
+DELETE FROM users 
+WHERE id = 2;
+```
+
 
 -- Удаление данных из таблицы
 DELETE FROM superheroes
