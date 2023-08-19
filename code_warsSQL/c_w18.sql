8eb1c367ab8c03a37b2dda7886ea4e251b0cd562
MihPar <9496260@gmail.com>
2023-08-19 22:28:36 +0300

    c_w181

--- code_warsSQL/c_w18.sql
+++ code_warsSQL/c_w18.sql
@@ -4,3 +4,9 @@ WHERE num + 1 NOT IN (
   SELECT num FROM data
 )
 LIMIT 1;
+
+---- Substitute with your SQL
+-- SELECT (n1.num + 1) AS missing_number
+-- FROM  data n1
+-- LEFT JOIN data n2 ON n1.num + 1 = n2.num
+-- WHERE n2.num IS NULL limit 1
